c22cf913ef4c102277fb98f76d7a51052ee96442
hughwilliams1 <hughwilliams2304@gmail.com>
2019-03-17 00:45:16 +0000

    Added Animations!

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,17 @@
 $(document).ready(function() {
+  var typed = new Typed(".type", {
+    strings: ["Hugh Williams"],
+    typeSpeed: 110,
+  });
+
+  sal({
+    threshold: 0.4,
+  });
   //Get Element
   var scrollLink = $('.scroll');
   //Smooth Scrolling
   scrollLink.click(function(e){
     e.preventDefault();
-    $('body, html').animate({scrollTop: $(this.hash).offset().top}, 1000);
+    $('body, html').animate({scrollTop: $(this.hash).offset().top}, 1500);
   })
-
 })
